c1452d2d9434627f8e81bcfd804a2c0adc0fdd56
Semih Uçar <smhucr10@gmail.com>
2023-08-27 16:51:18 +0300

    PlayerScripts

---
+++ GDCampHorrorGame/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //CameraStats
+    public float sensitivity;
+    private float xRotation = 0f;
+    private void Start()
+    {
+        //CursorINVisible
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        //Sensitivity Changer
+        //sensitivity = PlayerPrefs.GetFloat("MouseSensitivity", 200);
+        //Camera Look
+        transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity, 0);
+        xRotation -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        //Camera Border
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
---
+++ GDCampHorrorGame/Assets/Scripts/Story/StoryText.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+public class StoryText : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public string[] sentences;
+    private int index = 0;
+    public float textSpeed;
+
+    public UnityEvent Function;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            NextText();
+        }
+    }
+
+    private void OnEnable()
+    {
+        NextText();
+    }
+    private void NextText()
+    {
+        if (index <= sentences.Length - 1)
+        {
+            dialogueText.text = "";
+            StartCoroutine(WriteSentence());
+        }
+        else
+        {
+            Function.Invoke();
+        }
+    }
+    IEnumerator WriteSentence()
+    {
+
+        foreach (char Character in sentences[index].ToCharArray())
+        {
+            dialogueText.text += Character;
+            yield return new WaitForSeconds(textSpeed);
+
+        }
+        index++;
+        yield return new WaitForSeconds(0.8f);
+        NextText();
+    }
+
+    public void HellowWorld()
+    {
+        print("Ben bir fonksiyonum heehe");
+    }
+
+}
